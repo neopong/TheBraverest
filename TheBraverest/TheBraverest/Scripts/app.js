762e73311dca4cdb39acc9260ab720db68de8725
tt9 <tyler.thomas.127@gmail.com>
2015-08-25 18:53:06 -0700

    set up javascript on index page

---
+++ TheBraverest/TheBraverest/Scripts/app.js
@@ -0,0 +1,23 @@
+﻿
+(function (window, $, TheBraverest) {
+    
+    function submitBuildRequest() {
+        return $.ajax({
+            url: TheBraverest.actionGetBraveChampion,
+            method: 'GET',
+            dataType: 'JSON',
+            contentType: "application/json"
+        });
+    }
+
+
+    $('#submit-build-request').click(function (e) {
+        var test = e;
+        
+        $.when(submitBuildRequest())
+        .done(function (response) {
+            //TODO: display data from the thingy on the modal.
+        });
+    });
+
+})(window, jQuery, TheBraverest);
