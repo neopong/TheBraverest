1746506ad18bcec409239925badbe02a6872d180
tt9 <tyler.thomas.127@gmail.com>
2015-08-25 19:34:37 -0700

    Closures too difficult for simple object oriented programmer like me

--- TheBraverest/TheBraverest/Scripts/app.js
+++ TheBraverest/TheBraverest/Scripts/app.js
@@ -1,10 +1,14 @@
 ﻿
 (function (window, $, TheBraverest) {
+    var spinnerHtml = '<div class="throbber-loader">Loading…</div>';
+    var responseHtml = $('#response-modal .modal-body').html().toString();
     
+
     function submitBuildRequest() {
+        var actionData = TheBraverest.actionGetBraveChampion;
         return $.ajax({
-            url: TheBraverest.actionGetBraveChampion,
-            method: 'GET',
+            url: actionData.url,
+            method: actionData.method,
             dataType: 'JSON',
             contentType: "application/json"
         });
@@ -12,11 +16,13 @@
 
 
     $('#submit-build-request').click(function (e) {
-        var test = e;
-        
+        var replacement = responseHtml;
+        $('#response-modal .modal-body').html(spinnerHtml);
         $.when(submitBuildRequest())
         .done(function (response) {
             //TODO: display data from the thingy on the modal.
+            var test = response;
+
         });
     });
 
